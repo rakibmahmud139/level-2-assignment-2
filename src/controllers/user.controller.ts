f3f71ab9df1099553fc28c0db3d96b3bf4fcb643
Rakibmahmud <hasansaikat74@gmail.com>
2023-11-23 11:51:09 +0600

    create user route

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express';
+import { userServices } from '../services/user.services';
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const result = await userServices.createUser(userData);
+    res.status(200).json({
+      success: 'true',
+      message: 'user created successfully',
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: 'false',
+      message: error.message || 'something went wrong',
+    });
+  }
+};
+
+export const userRoutes = {
+  createUser,
+};
