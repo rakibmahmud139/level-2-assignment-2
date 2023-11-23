8f9191afa07ce20c020f213cd7e53d42b46c23bf
Rakibmahmud <hasansaikat74@gmail.com>
2023-11-23 11:07:29 +0600

    user interface added

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,22 @@
+export interface FullName {
+  firstName: string;
+  lastName: string;
+}
+
+export interface Address {
+  street: string;
+  city: string;
+  country: string;
+}
+
+export interface IUser {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: FullName;
+  age: number;
+  email: string;
+  isActive: 'true' | 'false';
+  hobbies: string[];
+  address: Address;
+}
