97dd792fc464b958cac06c5105582b6f809f126a
Rakibmahmud <hasansaikat74@gmail.com>
2023-11-23 11:42:51 +0600

    create user services

---
+++ src/services/user.services.ts
@@ -0,0 +1,11 @@
+import { IUser } from '../interface/user.interface';
+import User from '../models/user.model';
+
+const createUser = async (userData: IUser): Promise<IUser> => {
+  const result = await User.create(userData);
+  return result;
+};
+
+export const userServices = {
+  createUser,
+};
